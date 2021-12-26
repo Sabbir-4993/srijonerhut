344fd7cc23b976bc89e1d74a538689df5fa11ade
Sabbir Ahmed <sabbir15-4993@diu.edu.bd>
2021-12-26 16:37:35 +0600

    home modification added

--- resources/views/Frontend/component/banner.blade.php
+++ resources/views/Frontend/component/banner.blade.php
@@ -1,5 +1,4 @@
 <section class="banner-section">
-
     <div class="banner-carousel owl-carousel owl-theme">
 
         <!-- Slide Item -->
@@ -7,11 +6,11 @@
             <div class="auto-container">
                 <div class="content-box">
                     <span class="title">July 29, 2022</span>
-                    <h2> Bengali Film <br>Festival 2022</h2>
+                    <h2> Bengali cinema in <br> the Global landscape</h2>
                     <ul class="info-list">
-                        <li><span class="icon fa fa-chair"></span> 5000 Seats</li>
-                        <li><span class="icon fa fa-user-alt"></span> 12 Film</li>
-                        <li><span class="icon fa fa-map-marker-alt"></span> Dallas, Texas</li>
+                        <li><span class="icon fa fa-chair"></span> 3 Days</li>
+                        <li><span class="icon fa fa-user-alt"></span> 8 Film</li>
+                        <li><span class="icon fa fa-map-marker-alt"></span> Plano, Texas</li>
                     </ul>
                     <div class="btn-box"><a href="#" class="theme-btn btn-style-two"><span class="btn-title">Register Now</span></a></div>
                 </div>
@@ -22,12 +21,12 @@
         <div class="slide-item" style="background-image: url('{{asset("../../assets/images/main-slider/1.jpg")}}');">
             <div class="auto-container">
                 <div class="content-box">
-                    <span class="title">July 29, 2022</span>
-                    <h2> Bengali Film <br>Festival 2022</h2>
+                    <span class="title">July 30, 2022</span>
+                    <h2>বিশ্বের আঙিনায় <br> বাংলা সিনেমা</h2>
                     <ul class="info-list">
-                        <li><span class="icon fa fa-chair"></span> 5000 Seats</li>
-                        <li><span class="icon fa fa-user-alt"></span> 12 Film</li>
-                        <li><span class="icon fa fa-map-marker-alt"></span> Dallas, Texas</li>
+                        <li><span class="icon fa fa-chair"></span> 3 Days</li>
+                        <li><span class="icon fa fa-user-alt"></span> 8 Film</li>
+                        <li><span class="icon fa fa-map-marker-alt"></span> Plano, Texas</li>
                     </ul>
                     <div class="btn-box"><a href="#" class="theme-btn btn-style-two"><span class="btn-title">Register Now</span></a></div>
                 </div>
@@ -38,12 +37,12 @@
         <div class="slide-item" style="background-image: url('{{asset("../../assets/images/main-slider/2.jpg")}}');">
             <div class="auto-container">
                 <div class="content-box">
-                    <span class="title">July 29, 2022</span>
-                    <h2> Bengali Film <br>Festival 2022</h2>
+                    <span class="title">July 31, 2022</span>
+                    <h2>5th Bengali Film <br> Festival of Dallas 2022</h2>
                     <ul class="info-list">
-                        <li><span class="icon fa fa-chair"></span> 5000 Seats</li>
-                        <li><span class="icon fa fa-user-alt"></span> 12 Film</li>
-                        <li><span class="icon fa fa-map-marker-alt"></span> Dallas, Texas</li>
+                        <li><span class="icon fa fa-chair"></span> 3 Days</li>
+                        <li><span class="icon fa fa-user-alt"></span> 8 Film</li>
+                        <li><span class="icon fa fa-map-marker-alt"></span> Plano, Texas</li>
                     </ul>
                     <div class="btn-box"><a href="#" class="theme-btn btn-style-two"><span class="btn-title">Register Now</span></a></div>
                 </div>
--- resources/views/Frontend/component/performers.blade.php
+++ resources/views/Frontend/component/performers.blade.php
@@ -1,333 +1,174 @@
 <section class="speakers-section" style="background-image: url('{{asset("../../assets/images/background/6.jpg")}}');">
-
     <div class="auto-container">
-
         <div class="sec-title light text-center">
-
             <span class="title">Speakers</span>
-
-            <h2>Todays Performers</h2>
-
+            <h2>Today's Performers</h2>
         </div>
-
-
 
         <div class="row">
-
             <!-- Speaker Block -->
             <div class="speaker-block col-lg-4 col-md-6 col-sm-12">
-
                 <div class="inner-box">
-
                     <div class="image-box">
-
                         <figure class="image"><a href="speakers-detail.html"><img src="{{asset('assets/images/team/co-founder/Tariq.png')}}" alt=""></a></figure>
-
                     </div>
-
                     <div class="info-box">
-
                         <div class="inner">
-
                             <h4 class="name"><a href="speakers-detail.html">Tariq Yasin Uzwal</a></h4>
-
                             <span class="designation">Co Founder</span>
-
                             <ul class="social-links social-icon-colored">
-
                                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
-
                             </ul>
-
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
-
             <!-- Speaker Block -->
             <div class="speaker-block col-lg-4 col-md-6 col-sm-12">
-
                 <div class="inner-box">
-
                     <div class="image-box">
-
                         <figure class="image"><a href="speakers-detail.html"><img src="{{asset('assets/images/team/co-founder/Tanveer-Rahman.png')}}" alt=""></a></figure>
-
                     </div>
-
                     <div class="info-box">
-
                         <div class="inner">
-
                             <h4 class="name"><a href="speakers-detail.html">Tanveer Rahman</a></h4>
-
                             <span class="designation">Co Founder</span>
-
                             <ul class="social-links social-icon-colored">
-
                                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
-
                             </ul>
-
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
 
             <!-- Speaker Block -->
             <div class="speaker-block col-lg-4 col-md-6 col-sm-12">
-
                 <div class="inner-box">
-
                     <div class="image-box">
-
                         <figure class="image"><a href="speakers-detail.html"><img src="{{asset('assets/images/team/co-founder/Urmi-Adiba.png')}}" alt=""></a></figure>
-
                     </div>
-
                     <div class="info-box">
-
                         <div class="inner">
-
                             <h4 class="name"><a href="speakers-detail.html">Urmi Adiba</a></h4>
-
                             <span class="designation">Co Founder</span>
-
                             <ul class="social-links social-icon-colored">
-
                                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
-
                             </ul>
-
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
 
             <!-- Speaker Block -->
             <div class="speaker-block col-lg-3 col-md-6 col-sm-12">
-
                 <div class="inner-box">
-
                     <div class="image-box">
-
                         <figure class="image"><a href="speakers-detail.html"><img src="{{asset('../../assets/images/resource/speaker-4.jpg')}}" alt=""></a></figure>
-
                     </div>
-
                     <div class="info-box">
-
                         <div class="inner">
-
                             <h4 class="name"><a href="speakers-detail.html">Mitchell Heggestad</a></h4>
-
                             <span class="designation">Event Manager</span>
-
                             <ul class="social-links social-icon-colored">
-
                                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
-
                             </ul>
-
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
 
             <!-- Speaker Block -->
             <div class="speaker-block col-lg-3 col-md-6 col-sm-12">
-
                 <div class="inner-box">
-
                     <div class="image-box">
-
                         <figure class="image"><a href="speakers-detail.html"><img src="{{asset('../../assets/images/resource/speaker-5.jpg')}}" alt=""></a></figure>
-
                     </div>
-
                     <div class="info-box">
-
                         <div class="inner">
-
                             <h4 class="name"><a href="speakers-detail.html">Kenyetta Lesley</a></h4>
-
                             <span class="designation">Event Manager</span>
-
                             <ul class="social-links social-icon-colored">
-
                                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
-
                             </ul>
-
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
-
             <!-- Speaker Block -->
             <div class="speaker-block col-lg-3 col-md-6 col-sm-12">
-
                 <div class="inner-box">
-
                     <div class="image-box">
-
                         <figure class="image"><a href="speakers-detail.html"><img src="{{asset('../../assets/images/resource/speaker-6.jpg')}}" alt=""></a></figure>
-
                     </div>
-
                     <div class="info-box">
-
                         <div class="inner">
-
                             <h4 class="name"><a href="speakers-detail.html">Shelly Verghese</a></h4>
-
                             <span class="designation">Event Manager</span>
-
                             <ul class="social-links social-icon-colored">
-
                                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
-
                             </ul>
-
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
-
             <!-- Speaker Block -->
             <div class="speaker-block col-lg-3 col-md-6 col-sm-12">
-
                 <div class="inner-box">
-
                     <div class="image-box">
-
                         <figure class="image"><a href="speakers-detail.html"><img src="{{asset('../../assets/images/resource/speaker-7.jpg')}}" alt=""></a></figure>
-
                     </div>
-
                     <div class="info-box">
-
                         <div class="inner">
-
                             <h4 class="name"><a href="speakers-detail.html">Cassandra Kopka</a></h4>
-
                             <span class="designation">Event Manager</span>
-
                             <ul class="social-links social-icon-colored">
-
                                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
-
                             </ul>
-
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
-
             <!-- Speaker Block -->
             <div class="speaker-block col-lg-3 col-md-6 col-sm-12">
-
                 <div class="inner-box">
-
                     <div class="image-box">
-
-                        <figure class="image"><a href="speakers-detail.html"><img src="{{asset('../../assets/images/resource/speaker-8.jpg')}}" alt=""></a></figure>
-
+                        <figure class="image"><a href="speakers-detail.html"><img src="{{asset('../../assets/images/resource/speaker-8.jpg')}}" alt=""></a></figure
                     </div>
-
                     <div class="info-box">
-
                         <div class="inner">
-
                             <h4 class="name"><a href="speakers-detail.html">Eugene Clumpner</a></h4>
-
                             <span class="designation">Event Manager</span>
-
                             <ul class="social-links social-icon-colored">
-
                                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
-
                                 <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
-
                             </ul>
-
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
-
         </div>
-
     </div>
-
 </section>
